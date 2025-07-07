249bdddd612cca10e0caedb2541c5b25d1738fc0
areeb <areeb555@gmail.com>
2025-07-07 11:01:05 +0500

    Create Weather App for Use Live API

---
+++ app.js
@@ -0,0 +1,57 @@
+
+
+function SearchBtn() {
+    let inputData = document.getElementById('inputData').value.trim();
+    let print = document.getElementById("print");
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputData}&units=metric&appid=1bbfa7f12769064bb5a92915efd3fb09`)
+    .then(res => res.json())
+    .then((data) => {
+        console.log(data)
+        let typeEl = `
+        <section class="w-55">
+        <div class=" d-flex justify-content-center py-1">
+        <div class="col-md-8 col-lg-6 col-xl-5">
+        <div class="card text-body" style="border-radius: 35px;">
+        <div class="card-body p-4">
+        <div class="d-flex">    
+        <h6 class="flex-grow-1">City: ${data.name}, ${data.sys.country}</h6>
+        
+        </div>
+        <br/>
+
+        <div class="d-flex justify-content-center">
+            <img src="https://cdn.dribbble.com/userupload/25398855/file/original-b39f61791073c4efb3ba40bf4167eb31.gif" width="160px" class="rounded">
+        </div>
+
+        <div class="d-flex flex-column text-center mt-5 mb-4">
+        <h6 class="display-4 mb-0 font-weight-bold">${data.main.temp}°C</h6>
+        <p class="display-9 mb-0 "><b>Weather:</b> ${data.weather[0].main}</p>
+        <p class="display-9 mb-0 "><b>feels_like:</b> ${data.main.feels_like}</p>
+        
+        </div>
+        <div class="d-flex align-items-center">
+        <div class="flex-grow-1" style="font-size: 1rem;">
+        <div><i class="fas fa-wind fa-fw" style="color: #868B94;"></i> <span class="ms-1"><b>Speed:</b> ${data.wind.speed} km/h</span></div>
+        <div><i class="fas fa-tint fa-fw" style="color: #868B94;"></i> <span class="ms-1"><b>Humidity:</b> ${data.main.humidity}%</span></div>
+        </div>
+        <div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+
+        </section>
+        `
+
+        print.innerHTML = typeEl;
+    })
+    .catch((err) => {
+        print.innerHTML = `<p style="color: red; text-align: center;">Error: City not found or network issue.</p>`;
+        console.error("Error fetching weather data:", err);
+        
+            // inputData.value = "";
+    });
+}
